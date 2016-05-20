3e59fe4caaa9365bae0e09c90e68eab0001b4178
James Florentino <jamesflorentino@gmail.com>
2016-05-21 01:59:24 +0800

    use hiraya.js as entry point

---
+++ hiraya.js
@@ -0,0 +1,13 @@
+import Entity from './src/entity'
+import EntityState from './src/entity_state'
+import FiniteStateMachine from './src/entity_state'
+/**
+ * @module hiraya
+ */
+const hiraya = {
+  Entity: Entity,
+  EntityState: EntityState,
+  FiniteStateMachine: FiniteStateMachine
+}
+
+export default hiraya
