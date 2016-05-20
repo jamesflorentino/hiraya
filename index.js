36834074449e35a356dd03fcf4000bdbe67bc8e6
James Florentino <jamesflorentino@gmail.com>
2016-05-21 01:58:41 +0800

    deleted index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @module hiraya
- */
-module.exports = {
-  Entity: require('./lib/entity'),
-  EntityState: require('./lib/entity_state'),
-  FSM: require('./lib/finite_state_machine'),
-  Point: require('./lib/Point')
-}
